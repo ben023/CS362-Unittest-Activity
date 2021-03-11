311ccbf01eaede58620cf095d654b082247f1ab7
Ben <benlee1218@gmail.com>
2021-03-10 23:22:32 -0800

    implemented fizzBuzz method to pass first test

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,2 +1,10 @@
 def fizzBuzz():
-    pass
+    x = ""
+    for i in range(1,101):
+        if (i % 3 == 0):
+            x+= "Fizz "
+        elif (i % 5 == 0):
+            x+= "Buzz "
+        else:
+            x+= (str(i) + " ")
+    return x
