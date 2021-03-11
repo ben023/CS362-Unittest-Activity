c3d6a94e6ce2f5e98fec7624efb75979a7888caf
Ben <benlee1218@gmail.com>
2021-03-10 23:38:30 -0800

    implementation of leapYear that fails test 2

--- leapYear.py
+++ leapYear.py
@@ -2,7 +2,7 @@
 # Enter a number when requested for user input, and it will print out whether it is a leap year or not.
 
 def leapYear(year):
-    if (year % 4 == 0 and year % 100 !=0) or (year % 400 ==0):
+    if (year % 4 == 0 and year % 100 !=0):
         return("It is a leap year.")
     else:
         return("It is not a leap year.")
