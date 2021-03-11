e4a87bf64b37c672b8a013dec17dcb039c384896
Ben <benlee1218@gmail.com>
2021-03-10 23:39:02 -0800

    wrote second test, which leapYear currently fails

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,6 +4,7 @@ import leapYear
 class testLeapYear(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapYear.leapYear(2),"It is not a leap year.")
-        
+    def test2(self):
+        self.assertEqual(leapYear.leapYear(400), "It is a leap year.")
 if __name__ == '__main__':
     unittest.main()
